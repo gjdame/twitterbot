57be59291a47539dc29b810e292e1c0b79537863
Greg <gregoryjdame@gmail.com>
2018-01-12 00:44:38 +0000

    bot update

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'nStfo712PKCtXqRpbxnoRTAJn',  
+  consumer_secret: 'U8avC8EwNwzaAvTpluTZNVi5iMmmGyyOb8BwbihBtG0uzr1ddS',
+  access_token: '951577779320926208-7CvUbYKOdItcwjwstA2ytFbpgTqSkBp',  
+  access_token_secret: 'rDj09tgQ8wFpGFB9wNY9RvnniH0WeJ2TzyzEZTt93RdCI'
+}
